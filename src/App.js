f4a47e05856d83da2ab6bcf82833dfaa17a0de5c
Jiecai Peng <percy.pjc@gmail.com>
2021-08-01 22:06:57 -0400

    Seprate directory item into individual component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from "./Component/Homepage/homepage.component";
+import HomePage from "./Pages/homepage/homepage.component";
 
 function App() {
   return (
