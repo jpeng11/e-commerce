e397b430d2aa503f1710ef7e10cd4199be7b3e48
Percy <percy.pjc@gmail.com>
2021-08-14 18:00:14 -0400

    Added sign in page

--- src/Pages/homepage/homepage.component.jsx
+++ src/Pages/homepage/homepage.component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 import Directory from "../../Components/directory/directory.component";
-import Header from "../../Components/header/header.component";
 
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <Header />
     <Directory />
   </div>
 );
