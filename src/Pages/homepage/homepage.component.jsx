63e50ef7b66fbe23edf70a28c54fb738aa2780cf
Percy <percy.pjc@gmail.com>
2021-08-14 16:03:14 -0400

    Added header

--- src/Pages/homepage/homepage.component.jsx
+++ src/Pages/homepage/homepage.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Directory from "../../Components/directory/directory.component";
+import Header from "../../Components/header/header.component";
 
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
+    <Header />
     <Directory />
   </div>
 );
