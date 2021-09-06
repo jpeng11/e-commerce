3dbf11428e7235b96f1f9ffbd117c4e59178eae1
Percy <percy.pjc@gmail.com>
2021-09-05 20:19:52 -0400

    Impletement cart drop down

--- src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    {cartItems.map((item) => (
+      <div className="cart-items" key={item.id}>
+        {item.name}
+      </div>
+    ))}
+
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
