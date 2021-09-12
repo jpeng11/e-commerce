a09f39bdec8ae4ce8704e4920f62d1e198436a46
Percy <percy.pjc@gmail.com>
2021-09-12 14:44:36 -0400

    Moved shop data to redux

---
+++ src/Pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CollectionItem from "../../Components/collection-item/collection-item.component";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>CATEGORY PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
